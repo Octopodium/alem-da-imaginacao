e5847fd4f08f03e22009685234b13b3234548342
Juan Vitor <juanriven10@gmail.com>
2025-12-17 12:06:27 -0300

    🖱️Input System no GameManager

---
+++ AlemDaImaginacao/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+
+    public Actions actions;
+
+
+
+    public void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        actions = new Actions();
+        actions.Enable();
+    }
+}
